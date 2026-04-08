82f21e56cb39e1779b3fca0bd98093c82f4ae706
Fr33styler <Fr33stylerMC@gmail.com>
2026-04-08 21:38:32 +0300

    Chore: Added missing @Repository for UserHistoryRepository

--- src/main/java/ro/fr33styler/springbootcalculator/calculator/history/UserHistoryRepository.java
+++ src/main/java/ro/fr33styler/springbootcalculator/calculator/history/UserHistoryRepository.java
@@ -1,7 +1,9 @@
 package ro.fr33styler.springbootcalculator.calculator.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserHistoryRepository extends JpaRepository<UserHistory, Long> {
 
     UserHistory getUserHistoryByUsername(String username);
